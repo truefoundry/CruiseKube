94de669a71cad3fff6a64f6fb35218e0d63e1f10
Maanas Singh <59373661+maanas-23@users.noreply.github.com>
2026-01-28 13:09:52 +0530

    use dev api for apply recommendations e2e test (#72)
    * use dev api for apply recommendations e2e test
    
    * values fix
    
    * cleanup
    
    * update limit values
    
    * cleanup
    
    * cleanup

--- pkg/task/utils/validation.go
+++ pkg/task/utils/validation.go
@@ -5,27 +5,9 @@ const MinimumCPURecommendation = 0.001
 const MinimumMemoryRecommendation = 16.0
 
 func EnforceMinimumCPU(cpu float64) float64 {
-	if cpu != cpu {
-		return MinimumCPURecommendation
-	}
-	if cpu < 0 {
-		return MinimumCPURecommendation
-	}
-	if cpu < MinimumCPURecommendation {
-		return MinimumCPURecommendation
-	}
-	return cpu
+	return max(MinimumCPURecommendation, cpu)
 }
 
 func EnforceMinimumMemory(memory float64) float64 {
-	if memory != memory {
-		return MinimumMemoryRecommendation
-	}
-	if memory < 0 {
-		return MinimumMemoryRecommendation
-	}
-	if memory < MinimumMemoryRecommendation {
-		return MinimumMemoryRecommendation
-	}
-	return memory
+	return max(MinimumMemoryRecommendation, memory)
 }
