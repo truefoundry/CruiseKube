626a831300a65e0b7cf7a05cd7c88768fd5910f6
Maanas M Singh <maanasmsingh@gmail.com>
2026-02-19 11:56:04 +0530

    revert deletion of api: /api/v1/clusters/default/workloads

--- pkg/handlers/constants.go
+++ pkg/handlers/constants.go
@@ -2,6 +2,6 @@ package handlers
 
 import "time"
 
-// StatsAPIDataLookbackWindow is the time window for /stats and /recommendation-analysis APIs.
+// StatsAPIDataLookbackWindow is the time window for /stats, /workloads and /recommendation-analysis APIs.
 // Only data updated within this window is returned.
 const StatsAPIDataLookbackWindow = 24 * time.Hour
---
+++ pkg/handlers/ui.go
@@ -0,0 +1,81 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/truefoundry/cruisekube/pkg/logging"
+	"github.com/truefoundry/cruisekube/pkg/repository/storage"
+	"github.com/truefoundry/cruisekube/pkg/types"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ListWorkloadsHandler(c *gin.Context) {
+	ctx := c.Request.Context()
+	clusterID := c.Param("clusterID")
+	logging.Infof(ctx, "Listing workloads for cluster %s", clusterID)
+	// Only return workloads with data updated in the last 24 hours
+	since := time.Now().Add(-StatsAPIDataLookbackWindow)
+	stats, err := storage.Stg.GetAllStatsForClusterUpdatedSince(clusterID, since)
+	if err != nil {
+		logging.Errorf(ctx, "Failed to get stats for cluster %s: %v", clusterID, err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("Failed to get workloads for cluster %s: %v", clusterID, err),
+		})
+		return
+	}
+
+	var workloads = make([]types.WorkloadOverrideInfo, 0)
+	for _, stat := range stats {
+		if stat.IsGPUWorkload() {
+			continue
+		}
+		workloadColumnId := strings.ReplaceAll(stat.WorkloadIdentifier, "/", ":")
+		overrides, err := storage.Stg.GetWorkloadOverrides(clusterID, workloadColumnId)
+		if err != nil {
+			logging.Errorf(ctx, "Failed to get overrides for workload %s: %v", stat.WorkloadIdentifier, err)
+		}
+
+		evictionRanking := stat.EvictionRanking
+		enabled := true
+		var disruptionWindows []types.DisruptionWindow
+		if overrides != nil {
+			if overrides.EvictionRanking != nil {
+				evictionRanking = *overrides.EvictionRanking
+			}
+			if overrides.Enabled != nil {
+				enabled = *overrides.Enabled
+			}
+			if len(overrides.DisruptionWindows) > 0 {
+				disruptionWindows = overrides.DisruptionWindows
+			}
+		}
+
+		workloadExternalId := strings.ReplaceAll(stat.WorkloadIdentifier, "/", ":")
+		workload := types.WorkloadOverrideInfo{
+			WorkloadID: workloadExternalId,
+			Name:       stat.Name,
+			Namespace:  stat.Namespace,
+			Kind:       stat.Kind,
+			Overrides: &types.WorkloadOverridesEffective{
+				EvictionRanking:   evictionRanking,
+				Enabled:           enabled,
+				DisruptionWindows: disruptionWindows,
+			},
+		}
+		workloads = append(workloads, workload)
+	}
+
+	c.Header("Content-Type", "application/json")
+	if err := json.NewEncoder(c.Writer).Encode(workloads); err != nil {
+		logging.Errorf(ctx, "Failed to encode workloads: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("Failed to encode workloads: %v", err),
+		})
+		return
+	}
+}
