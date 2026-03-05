a9a50f45ef42232370b96585848c3612ff7b3af4
Raman Tehlan <ramantehlan@gmail.com>
2026-03-05 16:22:22 +0530

    fix: make coverage pod count aware

--- pkg/handlers/overview.go
+++ pkg/handlers/overview.go
@@ -173,12 +173,12 @@ func (deps HandlerDependencies) OverviewHandler(c *gin.Context) {
 
 	for i := range details {
 		d := details[i]
-		totalRequestedCPU += d.CPU.CurrentPerPod
-		totalRequestedMem += d.Memory.CurrentPerPod
+		totalRequestedCPU += d.CPU.CurrentPerPod * float64(d.PodsCount)
+		totalRequestedMem += d.Memory.CurrentPerPod * float64(d.PodsCount)
 		if d.Config.CruiseEnabled {
 			enabledWorkloads++
-			enabledRequestedCPU += d.CPU.CurrentPerPod
-			enabledRequestedMem += d.Memory.CurrentPerPod
+			enabledRequestedCPU += d.CPU.CurrentPerPod * float64(d.PodsCount)
+			enabledRequestedMem += d.Memory.CurrentPerPod * float64(d.PodsCount)
 		}
 	}
 
