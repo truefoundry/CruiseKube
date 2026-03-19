166ac03f86a59c1a18af23217b15793b35ae1c04
Raman Tehlan <ramantehlan@gmail.com>
2026-03-19 19:10:59 +0530

    fix: don't update recommendation generated for nonOptimizable workloads (#191)
    * fix: don't update recommendation generated for nonOptimizable workloads
    
    * feat: set to original values for unoptimizable
    
    ---------
    
    Co-authored-by: Shubham Rai <shubham.010793@gmail.com>

--- pkg/task/taskApplyRecommendation.go
+++ pkg/task/taskApplyRecommendation.go
@@ -744,17 +744,38 @@ func (a *ApplyRecommendationTask) buildAndSaveSnapshot(ctx context.Context, node
 	return nil
 }
 
+// nonOptimizablePodKeySet returns namespace/name keys for pods that must not receive stored recommendations.
+func nonOptimizablePodKeySet(pods []utils.PodInfo) map[string]struct{} {
+	m := make(map[string]struct{}, len(pods))
+	for _, p := range pods {
+		m[utils.GetPodKey(p.Namespace, p.Name)] = struct{}{}
+	}
+	return m
+}
+
 func (a *ApplyRecommendationTask) buildPodRecommendationRows(ctx context.Context, recommendationResults []*RecommendationResult) []types.PodResourceRecommendationRow {
 	rows := make([]types.PodResourceRecommendationRow, 0)
 	for _, res := range recommendationResults {
 		nodeName := res.NodeName
 		allocatableCPU := res.NodeInfo.AllocatableCPU
+		nonOptKeys := nonOptimizablePodKeySet(res.NonOptimizablePods)
 
 		for _, rec := range res.PodContainerRecommendations {
 			kind, namespace, name := rec.PodInfo.WorkloadKind, rec.PodInfo.Namespace, rec.PodInfo.WorkloadName
 			workloadID := utils.GetWorkloadKey(kind, namespace, name)
 			cpuRequest, memoryRequest, cpuLimit, memoryLimit := utils.ComputeRecommendedResourceValues(ctx, rec, allocatableCPU)
-
+			if _, skip := nonOptKeys[utils.GetPodKey(rec.PodInfo.Namespace, rec.PodInfo.Name)]; skip {
+				// if pod is non-optimizable, we don't compute recommended values
+				for _, container := range rec.PodInfo.ContainerResources {
+					if container.Name == rec.ContainerName {
+						memoryLimit = container.MemoryLimit
+						cpuRequest = container.CPURequest
+						cpuLimit = container.CPULimit
+						memoryRequest = container.MemoryRequest
+						break
+					}
+				}
+			}
 			payload := types.PodResourceRecommendation{
 				CPURequest:    cpuRequest,
 				MemoryRequest: memoryRequest,
